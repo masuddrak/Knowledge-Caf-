ca9f9abd89bcf303edef42c9008c7c7397053566
masud <masudranaacb@gmail.com>
2024-03-13 23:34:44 +0600

    project setup with tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./componnets/Header/Header"
+
+function App() {
+
+
+  return (
+    <div>
+      <Header></Header>
+    </div>
+  )
+}
+
+export default App
---
+++ src/componnets/Header/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-4xl text-center bg-slate-500 p-4 font-bold text-white'>React TailwindCSS</h1>
+        </div>
+    );
+};
+
+export default Header;
